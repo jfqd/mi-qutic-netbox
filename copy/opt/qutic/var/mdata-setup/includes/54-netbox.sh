3a157f7beb7a88a7cad221ca6fd8f91ca698c7b1
jfqd <jfqd@blun.org>
2020-07-23 17:00:31 +0200

    create random database pwd if config is missing

--- copy/opt/qutic/var/mdata-setup/includes/54-netbox.sh
+++ copy/opt/qutic/var/mdata-setup/includes/54-netbox.sh
@@ -4,16 +4,6 @@ SECRET_KEY=$(/home/netbox/netbox/netbox/generate_secret_key.py)
 sed -i "s#secret-key#${SECRET_KEY}#" \
   /home/netbox/netbox/netbox/netbox/configuration.py
 
-if mdata-get netbox_db_password 1>/dev/null 2>&1; then
-  DB_PWD=`mdata-get netbox_db_password`
-  sed -i "s#netbox-db-password#${DB_PWD}#" \
-    /home/netbox/netbox/netbox/netbox/configuration.py
-  # setup database
-  sudo -u postgres psql -c "CREATE DATABASE netbox;"
-  sudo -u postgres psql -c "CREATE USER netbox WITH PASSWORD '${DB_PWD}';"
-  sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE netbox TO netbox;"
-fi
-
 HOSTNAME=$(/usr/bin/hostname)
 sed -i "s#netbox.example.com#${HOSTNAME}#" \
   /home/netbox/netbox/netbox/netbox/configuration.py
@@ -27,6 +17,22 @@ fi
 # set admin email
 sed -i "s/# ['John Doe', 'jdoe@example.com'],/['admin', '${ADMIN_EMAIL}']/" \
   /home/netbox/netbox/netbox/netbox/configuration.py
+
+# get db password
+if mdata-get netbox_db_password 1>/dev/null 2>&1; then
+  DB_PWD=`mdata-get netbox_db_password`
+else
+  DB_PWD=$(/opt/local/bin/gpg --gen-random --armor 1 24 2>/dev/null)
+fi
+
+# set db password
+sed -i "s#netbox-db-password#${DB_PWD}#" \
+  /home/netbox/netbox/netbox/netbox/configuration.py
+
+# setup database
+sudo -u postgres psql -c "CREATE DATABASE netbox;"
+sudo -u postgres psql -c "CREATE USER netbox WITH PASSWORD '${DB_PWD}';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE netbox TO netbox;"
 
 # migrate db
 cd /home/netbox/netbox/netbox
